30ae360163a6cbdf7ffccd0b8431039105c82db7
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-09-19 12:17:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return (Math.abs(someValue - 42))
+  }
+  function distanceFromHqInFeet(someValue) {
+    return (264 * distanceFromHqInBlocks(someValue));
+  }
+  function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264;
+    } else {
+        return (start - destination) * 264;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if (distanceInFeet < 1) {
+        return 'Distance cannot be zero or negative';
+    } else if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
